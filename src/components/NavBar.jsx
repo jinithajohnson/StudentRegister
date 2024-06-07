1c47ad16e079df67033f31ddfa545b46eddf4325
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-07 22:06:43 +0530

    form

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
@@ -6,26 +7,26 @@ const NavBar = () => {
 
 <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Student App</a>
+    <Link class="navbar-brand" to="#">Student App</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Student Entry</a>
+          <Link class="nav-link active" aria-current="page" href="">Student Entry</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/">Add Student </a>
+          <Link class="nav-link" to="/">Add Student </Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/Search">Search</a>
+          <Link class="nav-link" to="/Search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/Delete">Delete Student</a>
+          <Link class="nav-link" to="/Delete">Delete Student</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/View">View Student details</a>
+          <Link class="nav-link" to="/View">View Student details</Link>
         </li>
       </ul>
     </div>
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const View = () => {
+
+        const [data,changeData]=useState(
+
+                [
+
+                        {"name":"aju","age":21},
+                        {"name":"aj","age":21},
+                        {"name":"ajdw","age":21}
+                        
+
+
+                ]
+
+        )
+
     return (
         <div>
                 <NavBar/>
@@ -10,61 +25,33 @@ const View = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwpk3CKQTBBPBfwazClhc7eP-mW7mVEpESaQ&s" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Jisna</h5>
-                                        <p class="card-text">Roll no:5</p>
-                                        <a href="#" class="btn btn-primary">View details</a>
-                                    </div>
-                            </div>
-
-
-
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwpk3CKQTBBPBfwazClhc7eP-mW7mVEpESaQ&s" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Jisna</h5>
-                                        <p class="card-text">Roll no:5</p>
-                                        <a href="#" class="btn btn-primary">View details</a>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-                        
-                                <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                           
+                           {data.map(
+                                (value,index)=>{
+                                    return  <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                     <div className="card">
                                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwpk3CKQTBBPBfwazClhc7eP-mW7mVEpESaQ&s" class="card-img-top" alt="..." />
                                         <div class="card-body">
-                                            <h5 class="card-title">Jisna</h5>
-                                            <p class="card-text">Roll no:5</p>
+                                            <h5 class="card-title">{value.name}</h5>
+                                            <p class="card-text"></p>
                                             <a href="#" class="btn btn-primary">View details</a>
                                         </div>
+                                </div>
+    
+                                </div>
+                           
+                                }
 
 
-                                    </div>
-                                    </div>
-                                    <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                           )}
+                            
 
-                                        <div className="card">
-                                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwpk3CKQTBBPBfwazClhc7eP-mW7mVEpESaQ&s" class="card-img-top" alt="..." />
-                                            <div class="card-body">
-                                                <h5 class="card-title">Jisna</h5>
-                                                <p class="card-text">Roll no:5</p>
-                                                <a href="#" class="btn btn-primary">View details</a>
-                                            </div>
-                                        </div>
+
+
                         
-                                   </div>
+                               
+                                    
                         </div>
                     </div>
                 </div>
